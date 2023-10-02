b64cb278cdb0e1e697177a3a5cc0a6a6acc0aa49
andres <aca00115@red.ujaen.es>
2023-10-02 10:07:22 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AndresCano_WordCounter_LAB2"
+include(":app")
+ 
